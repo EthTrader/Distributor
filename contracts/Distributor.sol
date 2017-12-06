0e55bd322c98fa37f0e73e97ebfc47a3a279f887
Carl Larson <cslarson@gmail.com>
2017-12-06 10:41:35 +0000

    check for array length

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.18;
 
 contract Distributor {
     function sendTo(address[] recipients) public payable {
+        require(recipients.length != 0);
         uint each = msg.value/(recipients.length);
         for (uint i = 0; i < recipients.length; i++) {
           require(recipients[i].send(each));
