51ca4a682f75cf163d5743dcb2ccffedaf13a3a1
Carl Larson <cslarson@gmail.com>
2017-12-08 10:48:07 +0000

    set test gas

--- truffle.js
+++ truffle.js
@@ -5,7 +5,8 @@ module.exports = {
     test: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 5000000
     },
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
